7d23663d1c8adf7859ad2526808332f295d1aca0
Richard Stephenson <richard.stephenson@moneycorp.com>
2018-09-05 10:40:35 +0100

    Add basic redux store.

--- src/index.js
+++ src/index.js
@@ -3,4 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from 'react-redux'
+
+import configureStore from './store';
+
+const initialState = {};
+const store = configureStore(initialState)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
